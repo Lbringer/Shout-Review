8aad91c5fcfdefcb8ab453c125f9098c898ff531
Aditya <g.aditya2307@gmail.com>
2024-01-05 11:49:22 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+## Movie Review API to understand spring boot and JPA
---
+++ src/test/java/com/api/shoutReview/ShoutReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.shoutReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoutReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
